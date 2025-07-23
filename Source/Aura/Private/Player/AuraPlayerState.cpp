8ba6d0781e84e45c9c6cbae3092c4c6c353777d6
HuiH11 <kkmo9407@gmail.com>
2025-07-23 16:54:34 +0900

    attributeset

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -5,6 +5,7 @@
 
 #include "AttributeSet.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
 
 
 AAuraPlayerState::AAuraPlayerState()
@@ -13,7 +14,7 @@ AAuraPlayerState::AAuraPlayerState()
     AbilitySystemComponent->SetIsReplicated(true);
     AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
 
-    AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
+    AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
     
     NetUpdateFrequency = 100.0f;
 }
